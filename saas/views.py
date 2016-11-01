fda2f0d66ab6590a5849be072330dc89ea1bb2be
muroslav2909 <myroslav@preply.com>
2016-11-01 22:48:21 +0200

    loginggg

--- saas/views.py
+++ saas/views.py
@@ -10,11 +10,24 @@ def home(request):
         user = authenticate(username=email, password=password)
         if user is not None:
             return render(request, "auth/main.html", context)
+        else:
+            pass
+
     return render(request, "auth/login.html", context)
 
 def register(request):
     context = {}
+    if request.method == 'POST' and request.POST['email'] and request.POST['full_name'] and request.POST['password'] and request.POST['role']:
+        email = request.POST['email']
+        password = request.POST['password']
+        full_name = request.POST['full_name']
+        role = request.POST['role']
+        user = User(username=full_name, email=email, password=password)
+        user.save()
+        path = "auth/%s_register.html" % role
+        return render(request, path, context)
     return render(request, "auth/register.html", context)
+
 
 def parent_register(request):
     context = {}
