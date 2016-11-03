ebd999bf721d9af478ed9aaf2428c6485ff81803
muroslav2909 <myroslav@preply.com>
2016-11-03 20:34:13 +0200

    login_required

--- saas/views.py
+++ saas/views.py
@@ -5,21 +5,15 @@ from saas.forms import FirstStepRegistration, ParentRegistration, JudgesRegistra
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from saas.models import Parent, School, Judge, Admin
-# from django.contrib.auth import (
-#     REDIRECT_FIELD_NAME, get_user_model, login as auth_login,
-#     logout as auth_logout, update_session_auth_hash,
-# )
 
 def home(request):
     context = {}
-    # auth_logout(request)
     if request.method == 'POST' and request.POST['post']:
         email = request.POST['email']
         password = request.POST['password']
         auth = authenticate(username=email, password=password)
         if auth is not None:
             login(request, auth)
-            # auth_login(request, User.objects.get(username=email, password=password))
             return redirect("/main")
         else:
             context = {'er1': 'yes'}
@@ -27,7 +21,6 @@ def home(request):
 
 def register(request):
     context = {}
-    auth_logout(request)
     if request.method == 'POST' and request.POST['post']:
         form = FirstStepRegistration(request.POST)
         if form.is_valid():
@@ -37,20 +30,18 @@ def register(request):
             email = form.cleaned_data['email']
             auth = authenticate(username=email, password=password)
             if auth is not None:
-                print "auth is not None"
-                # login(request, auth)
-                # auth_login(request, User.objects.get(username=email, password=password))
+                login(request, auth)
                 return redirect("/main")
             user = User(username=email, email=email, password=password)
             user.save()
-            # auth = authenticate(username=email, password=password)
-            # login(request, auth)
             path = "%s_register" % role
+            login(request, user)
             return redirect(path)
     return render(request, "auth/register.html", context)
 
-# @login_required#(login_url='/')
+@login_required
 def parent_register(request):
+    print "request.userrequest.userrequest.userrequest.userrequest.userrequest.user", request.user
     schools = School.objects.all()
     context = {"schools": schools}
     if request.method == 'POST' and request.POST['post']:
@@ -63,16 +54,17 @@ def parent_register(request):
             state = form.cleaned_data['state']
             zipcode = form.cleaned_data['zipcode']
             phone = form.cleaned_data['phone']
-            #request.user
-            parent, created = Parent.objects.get_or_create(user=User.objects.filter()[0], defaults = {'mailing_address_1': mailing_address_1, 'mailing_address_2': mailing_address_2, 'state': state, 'city': city, 'zipcode': zipcode, 'phone': phone})
+            parent, created = Parent.objects.get_or_create(user=request.user, defaults = {'mailing_address_1': mailing_address_1, 'mailing_address_2': mailing_address_2, 'state': state, 'city': city, 'zipcode': zipcode, 'phone': phone})
             parent.school.add(school)
             parent.save()
+            # if not request.user.is_authenticated():
+            #     login(request, request.user)
             return redirect('/main')
         else:
             context = {'er1': 'yes', "schools": schools}
     return render(request, "auth/parent_register.html", context)
 
-# @login_required#(login_url='/')
+@login_required
 def judges_register(request):
     context = {}
     if request.method == 'POST' and request.POST['post']:
@@ -81,13 +73,14 @@ def judges_register(request):
             organisation = form.cleaned_data['organisation']
             phone = form.cleaned_data['phone']
             category = form.cleaned_data['category']
-            judge, created = Judge.objects.get_or_create(user=User.objects.filter()[0],
+            judge, created = Judge.objects.get_or_create(user=request.user,
                                                            defaults={'organisation': organisation,
                                                                      'phone': phone})
             judge.save()
             return redirect('/main')
     return render(request, "auth/judges_register.html", context)
 
+@login_required
 def chair_register(request):
     context = {}
     if request.method == 'POST' and request.POST['post']:
@@ -101,32 +94,33 @@ def chair_register(request):
             phone = form.cleaned_data['phone']
             school = School(name=form.cleaned_data['school'], address_1=address_1, address_2=address_2, city=city, state=state, zipcode=zipcode)
             school.save()
-            admin, created = Admin.objects.get_or_create(user=User.objects.filter()[0], defaults={'phone': phone})
+            admin, created = Admin.objects.get_or_create(user=request.user, defaults={'phone': phone})
             admin.school.add(school)
             admin.save()
 
             return redirect('/main')
     return render(request, "auth/chair_register.html", context)
 
-# @login_required
+@login_required
 def main(request):
     context = {}
+
     print "request.userrequest.userrequest.userrequest.userrequest.userrequest.user", request.user
-    # try:
-    #     p = Parent.objects.get(user=request.user)
-    # except: p = None
-    # try:
-    #     j = Judge.objects.get(user=request.user)
-    # except:
-    #     j = None
-    # try:
-    #     c = Admin.objects.get(user=request.user)
-    # except:
-    #     c = None
-    # if p:
-    #     return render(request, "auth/parent.html", context)
-    # if j:
-    #     return render(request, "auth/judges.html", context)
-    # if c:
-    return render(request, "auth/parent.html", context)
+    try:
+        p = Parent.objects.get(user=request.user)
+    except: p = None
+    try:
+        j = Judge.objects.get(user=request.user)
+    except:
+        j = None
+    try:
+        c = Admin.objects.get(user=request.user)
+    except:
+        c = None
+    if p:
+        return render(request, "auth/parent.html", context)
+    if j:
+        return render(request, "auth/judges.html", context)
+    if c:
+        return render(request, "auth/chair.html", context)
 
